3a0716f01519f00a108c64e9af9eb2711fb29ff3
Pijush2003 <98014641+Pijush2003@users.noreply.github.com>
2024-09-05 11:03:23 +0530

    Add files via upload

---
+++ tut53.js
@@ -0,0 +1,19 @@
+console.log("This is tutorial 53");
+function greet(name , grrettext) {
+    console.log(name +" is a good boy");
+    console.log(grrettext+""+name);
+}
+
+let name = "Harry";
+let name1 = "subha";
+let name2 = "gourab";
+let name3 = "soumen";
+let grrettext ="good morning";
+// console.log( name + " is a good teacher");
+// console.log( name1 + " is a good teacher");
+// console.log( name2 + " is a good teacher");
+// console.log( name3 + " is a good teacher");
+greet(name,grrettext);
+greet(name1,grrettext);
+greet(name2,grrettext);
+greet(name3,grrettext);
